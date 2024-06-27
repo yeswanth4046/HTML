cbc4f1d88f8813a3280c2301fdd20514c1d1f830
yeswanth4046 <yeswantheee24@gmail.com>
2024-06-27 10:33:53 +0530

    gji

--- sai143.py
+++ sai143.py
@@ -1,18 +1,7 @@
-<<<<<<< HEAD:sai143.py
-print("nani")
-print("sai")
-print ("yeswanth") 
-=======
-print("nani")
-print("sai")
-print ("yeswanth")
->>>>>>> 94f716b (ok):sai4046/sai143.py
-n
-a= 23
-b= 45
-print(a+b)
-#nn
-a=45
-b=56
-c=67
-print (a+b+c)
+#<<< HEAD:sai143.py
+n=int (input("number:"))
+i=1
+while(i<=10):
+    print(n,"*","i","=",n+1)
+    i=i+1
+    
